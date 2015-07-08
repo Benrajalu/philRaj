7d0fb87642ce32e38bd11a39e4980e40cd9e237e
rajalubenoit@gmail.com <rajalubenoit@gmail.com>
2015-07-08 22:39:53 +0200

    Automated commit at 2015-07-08 20:39:53 UTC by middleman-deploy 1.0.0

---
+++ js/plugins.js
@@ -0,0 +1,46 @@
+/*global $:false */
+/*global Modernizr:false */
+/*global console:false */
+/*global confirm:false */
+
+'use strict';
+
+// Avoid `console` errors in browsers that lack a console.
+if (!(window.console && console.log)) {
+    (function() {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }());
+}
+
+
+$(window).load(function () {
+  // Marking menu as active
+    var activeMenu = function(){
+      var menu = $('#mainNav'),
+          activeEntry = $(menu).find('.active'),
+          scale = $(menu).find('ul');
+
+        console.log(scale);
+
+      $(scale).append('<li class="activeMark"></li>');
+
+      var mark = $(scale).find('.activeMark');
+
+      $(mark).css({'width': $(activeEntry).outerWidth(), 'left' : $(activeEntry).position().left, 'opacity' : 1 });
+    }
+
+    activeMenu();
+
+  // Menu button
+    $('#toggleMenu').click(function(event){
+      event.preventDefault();
+      $(this).toggleClass('active');
+      $('#mainNav').find('ul').toggleClass('show');
+    })
+});
