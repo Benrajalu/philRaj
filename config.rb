7adc99842f131446473d0c0518e01ff5d44dd422
Benoit Rajalu <rajalubenoit@gmail.com>
2015-07-08 22:38:48 +0200

    Base index

---
+++ config.rb
@@ -0,0 +1,112 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'img'
+
+set :relative_links, true
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  #activate :minify_css
+
+  # Minify Javascript on build
+  #activate :minify_javascript
+
+  # Enable cache buster
+  #activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  #activate :directory_indexes
+
+  #activate :favicon_maker do |f|
+    #f.template_dir  = File.join(root, 'source')
+    #f.output_dir    = File.join(root, 'build')
+    #f.icons = {
+      #{}"favicon_template.png" => [
+        #{ icon: "apple-touch-icon-152x152-precomposed.png" },
+        #{ icon: "apple-touch-icon-114x114-precomposed.png" },
+        #{ icon: "apple-touch-icon-72x72-precomposed.png" },
+        #{ icon: "mstile-144x144", format: :png },
+        #{ icon: "favicon.png", size: "16x16" },
+        #{ icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      #]
+    #}
+  #end
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = "custom-remote" # remote name or git url, default: origin
+  # deploy.branch   = "custom-branch" # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+end
+
+helpers do
+    # Sets the html class to 'active' when the link url is equal to the current page being viewed.
+    # Use just like the link_to helper.
+    # <%= magic_link_to 'Home', '/index.html' %>
+    def magic_link_to(link, url, opts={})
+        current_url = current_resource.url
+        if current_url == "/" + url || current_url == "/" + url_for(url)
+            opts[:class] = "active"
+        end
+        link_to(link, url, opts)
+    end
+end
